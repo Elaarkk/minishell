aa3e48bbf9d15c3e6c4fa0b0a96cab2686993937
Aloun Colonne <acolonne@bess-f3r4s13.clusters.42paris.fr>
2025-03-24 19:50:56 +0100

    Moto + init

---
+++ includes/minishell.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acolonne <acolonne@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/24 16:32:33 by acolonne          #+#    #+#             */
+/*   Updated: 2025/03/24 18:53:59 by acolonne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h> 
+# include <unistd.h>
+# include <limits.h>
+# include <string.h>
+# include <sys/time.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif
